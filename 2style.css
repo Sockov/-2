5e05007996758762aa2893281a3af69a7832952f
Sockov <119696917+Sockov@users.noreply.github.com>
2023-01-14 11:37:59 +0300

    Create 2style.css

---
+++ 2style.css
@@ -0,0 +1,66 @@
+body {
+    font-family: sans-serif;
+}
+
+header {
+    background-color: #ADD8E6;
+}
+
+header img {
+    width: 90px;
+    height: 60px;
+}
+
+h1 {
+    width: 500px;
+    height: 50px;
+    color: blueviolet;
+}
+
+footer p {
+    background-color: rgb(53, 228, 53);
+    padding: 2px;
+    margin: 15px;
+}
+
+nav {
+    margin: 20px;
+    padding: 10px;
+    border: 4px solid rgb(34, 199, 199);
+}
+
+nav a {
+   text-decoration: none;
+   padding: 28px;
+}
+
+.lol {
+  background-color: rgb(211, 112, 54); 
+  border-radius: 15px;
+}
+
+.leon {
+  background-color: rgb(211, 112, 54); 
+  border-radius: 15px;
+}
+
+.shelly {
+  background-color: rgb(211, 112, 54); 
+  border-radius: 15px;
+}
+
+.bull {
+  background-color: rgb(211, 112, 54); 
+  border-radius: 15px;
+}
+
+h2 {
+  color: blue;
+}
+
+.hot {
+  border-radius: 20px;
+  border: 1px solid blue;
+}
+
+head
